700999e7c48c359022aba30639b44a00aed25004
Eduarda Nere <eduardanere890@gmail.com>
2025-07-18 01:08:32 -0300

    melhorando responsivo e outros

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,9 @@
-import { HeaderContainer, HeaderTitle } from './styles';
+import { HeaderContainer, HeaderTitle, HeaderLogo } from './styles';
 
 const Header = () => {
     return (
         <HeaderContainer>
+            <HeaderLogo src="imagens/logo.png" alt="Logo" />
             <HeaderTitle>Viva experiências gastronômicas no conforto da sua casa</HeaderTitle>
         </HeaderContainer>
     );
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,16 +1,23 @@
 import styled from 'styled-components';
 
 export const HeaderContainer = styled.header`
-    background-image: url('/imagens/hero.png');
+    background-image: url('/imagens/fundo.png');
     background-size: auto 100%;  
     background-repeat: repeat-x;
     background-position: center top;
     height: 384px;
     width: 100%;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
     text-align: center;
+`;
+
+export const HeaderLogo = styled.img`
+    width: 125px;
+    margin-bottom: 20px;
+    padding-bottom: 60px;
 `;
 
 export const HeaderTitle = styled.h1`
@@ -20,5 +27,5 @@ export const HeaderTitle = styled.h1`
     font-weight: 900;
     max-width: 540px;
     padding: 0 20px;
-    margin-top: 150px;
+    margin-top: 0;
 `;
