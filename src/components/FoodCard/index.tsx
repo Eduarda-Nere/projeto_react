048bbe45146c9697a28825118e13425c1232c782
Eduarda Nere Rodrigues <eduardanere890@gmail.com>
2025-07-21 22:01:17 -0300

    melhorando responsivo e outros

--- src/components/FoodCard/index.tsx
+++ src/components/FoodCard/index.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
 import * as S from './styles';
 
+interface FoodCardProps {
+    image: string;
+    name: string;
+    description: string;
+    onAddToCart: () => void;
+    onImageClick: () => void;
+}
+
 const FoodCard: React.FC<FoodCardProps> = ({
     image,
     name,
     description,
-    price,
-    serves,
     onAddToCart,
     onImageClick
 }) => {
     return (
         <S.FoodCardContainer>
-            <S.FoodImageWrapper onClick={onImageClick}>
-                <S.FoodImage src={image} />
-            </S.FoodImageWrapper>
-            <S.FoodContent>
-                <S.FoodName>{name}</S.FoodName>
-                <S.FoodDescription>{description}</S.FoodDescription>
-                <S.AddToCartButton onClick={onAddToCart}>
-                    Adicionar ao carrinho - R$ {price.toFixed(2).replace('.', ',')}
-                </S.AddToCartButton>
-            </S.FoodContent>
+        <S.FoodImageWrapper onClick={onImageClick}>
+            <S.FoodImage src={image} alt={name} />
+        </S.FoodImageWrapper>
+        <S.FoodContent>
+            <S.FoodName>{name}</S.FoodName>
+            <S.FoodDescription>{description}</S.FoodDescription>
+            <S.AddToCartButton onClick={onAddToCart}>
+                Adicionar ao carrinho
+            </S.AddToCartButton>
+        </S.FoodContent>
         </S.FoodCardContainer>
     );
 };
